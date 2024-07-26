16ffb08bc899f8e6aaba6759cbb4d0baf431b385
leestana01 <leestana01@naver.com>
2024-07-27 02:53:14 +0900

    [feat] issue-#4 : GameRoom 구현 완료

--- src/main/java/likelion/hufsglobal/lgtu/runwithmate/controller/GameRoomController.java
+++ src/main/java/likelion/hufsglobal/lgtu/runwithmate/controller/GameRoomController.java
@@ -4,7 +4,9 @@ import likelion.hufsglobal.lgtu.runwithmate.domain.gameroom.GameStartResDto;
 import likelion.hufsglobal.lgtu.runwithmate.domain.gameroom.RoomJoinResDto;
 import likelion.hufsglobal.lgtu.runwithmate.domain.gameroom.RoomUpdateReqDto;
 import likelion.hufsglobal.lgtu.runwithmate.domain.gameroom.RoomUpdateResDto;
+import likelion.hufsglobal.lgtu.runwithmate.service.GameRoomService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.http.ResponseEntity;
 import org.springframework.messaging.handler.annotation.DestinationVariable;
 import org.springframework.messaging.handler.annotation.MessageMapping;
@@ -12,55 +14,38 @@ import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 @Controller
 @RequiredArgsConstructor
 public class GameRoomController {
 
+    private final GameRoomService gameRoomService;
+
     @PostMapping("/api/games/join")
     public ResponseEntity<String> joinGameRoom() {
-
-        // 방 번호 생성 로직
-        return ResponseEntity.ok("A1B2C3");
-
+        String userId = "user1"; // 임시로 유저 이름 지정
+        return ResponseEntity.ok(gameRoomService.createRoom(userId));
     }
 
     @MessageMapping("/update_room/{roomId}")
     @SendTo("/room/{roomId}")
     public RoomUpdateResDto updateRoom(@DestinationVariable String roomId, RoomUpdateReqDto roomUpdateReqDto) {
-
-        // 방 정보 업데이트 로직
-        RoomUpdateResDto roomUpdateResDto = new RoomUpdateResDto();
-        roomUpdateResDto.setStatus(true);
-        roomUpdateResDto.setBetPoint(roomUpdateReqDto.getBetPoint());
-        roomUpdateResDto.setTimeLimit(roomUpdateReqDto.getTimeLimit());
-
-        return roomUpdateResDto;
+        String userId = "user1"; // 요청한 유저의 id를 받아와야 함
+        return gameRoomService.updateRoom(roomId, userId, roomUpdateReqDto.getBetPoint(), roomUpdateReqDto.getTimeLimit());
     }
 
     @MessageMapping("/join_room/{roomId}")
     @SendTo("/room/{roomId}")
     public RoomJoinResDto joinRoom(@DestinationVariable String roomId) {
 
-        // 방 참가 로직
-        RoomJoinResDto roomJoinResDto = new RoomJoinResDto();
-        roomJoinResDto.setUser1("user1");
-        roomJoinResDto.setUser2("user2");
-        roomJoinResDto.setBetPoint(1000L);
-        roomJoinResDto.setTimeLimit(60);
-
-        return roomJoinResDto;
+        String userId = "user2"; // 임시로 유저 이름 지정
+        return gameRoomService.joinRoom(roomId, userId);
     }
 
     @MessageMapping("/start_game/{roomId}")
     @SendTo("/room/{roomId}")
     public GameStartResDto startGame(@PathVariable String roomId) {
-
-        // 게임 시작 로직
-        GameStartResDto gameStartResDto = new GameStartResDto();
-        gameStartResDto.setRoomId(roomId);
-
-        return gameStartResDto;
+        // 박스 위치 정보는 GameController에서 모든 플레이어들이 게임 시작 요청하면, 위치정보를 받으면서 랜덤으로 박스 위치를 생성하여 전달
+        return gameRoomService.startGame(roomId);
     }
 }
---
+++ src/main/java/likelion/hufsglobal/lgtu/runwithmate/service/GameRoomService.java
@@ -0,0 +1,78 @@
+package likelion.hufsglobal.lgtu.runwithmate.service;
+
+import likelion.hufsglobal.lgtu.runwithmate.domain.gameroom.GameStartResDto;
+import likelion.hufsglobal.lgtu.runwithmate.domain.gameroom.RoomJoinResDto;
+import likelion.hufsglobal.lgtu.runwithmate.domain.gameroom.RoomUpdateResDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.stereotype.Service;
+
+import java.util.UUID;
+
+@Service
+@RequiredArgsConstructor
+public class GameRoomService {
+
+    private final RedisTemplate<String, Object> redisTemplate;
+    private static final Long DEFAULT_BET_POINT = 1000L;
+    private static final Integer DEFAULT_TIME_LIMIT = 60;
+
+    public String createRoom(String userId) {
+        // 방 ID 생성
+        String roomId = UUID.randomUUID().toString().substring(0, 6).toUpperCase();
+        while (Boolean.TRUE.equals(redisTemplate.hasKey("game_rooms:" + roomId))) {
+            roomId = UUID.randomUUID().toString().substring(0, 6).toUpperCase();
+        }
+        redisTemplate.opsForHash().put("game_rooms:" + roomId, "bet_point", DEFAULT_BET_POINT);
+        redisTemplate.opsForHash().put("game_rooms:" + roomId, "time_limit", DEFAULT_TIME_LIMIT);
+        redisTemplate.opsForHash().put("game_rooms:" + roomId, "user1_id", userId);
+        redisTemplate.opsForHash().put("game_rooms:" + roomId, "game_status", "room_created");
+
+        return roomId;
+    }
+
+    public RoomUpdateResDto updateRoom(String roomId, String userId, Long betPoint, Integer timeLimit) {
+        String userOneId = (String) redisTemplate.opsForHash().get("game_rooms:" + roomId, "user1_id");
+        if (!userId.equals(userOneId)) {
+            RoomUpdateResDto roomUpdateResDto = new RoomUpdateResDto();
+            roomUpdateResDto.setStatus(false);
+            roomUpdateResDto.setBetPoint((Long) redisTemplate.opsForHash().get("game_rooms:" + roomId, "bet_point"));
+            roomUpdateResDto.setTimeLimit((Integer) redisTemplate.opsForHash().get("game_rooms:" + roomId, "time_limit"));
+            return roomUpdateResDto;
+        }
+
+        redisTemplate.opsForHash().put("game_rooms:" + roomId, "bet_point", betPoint);
+        redisTemplate.opsForHash().put("game_rooms:" + roomId, "time_limit", timeLimit);
+
+        RoomUpdateResDto roomUpdateResDto = new RoomUpdateResDto();
+        roomUpdateResDto.setStatus(true);
+        roomUpdateResDto.setBetPoint(betPoint);
+        roomUpdateResDto.setTimeLimit(timeLimit);
+        return roomUpdateResDto;
+    }
+
+    public RoomJoinResDto joinRoom(String roomId, String userId) {
+        redisTemplate.opsForHash().put("game_rooms:" + roomId, "user2_id", userId);
+        String userOneId = (String) redisTemplate.opsForHash().get("game_rooms:" + roomId, "user1_id");
+        String userTwoId = userId;
+
+        String userOneName = "유저1"; // 나중에 userOneId로 이름을 가져와야 함
+        String userTwoName = "유저2"; // 나중에 userTwoId로 이름을 가져와야 함
+
+
+        RoomJoinResDto roomJoinResDto = new RoomJoinResDto();
+        roomJoinResDto.setUser1(userOneName);
+        roomJoinResDto.setUser2(userTwoName);
+        roomJoinResDto.setBetPoint((Long) redisTemplate.opsForHash().get("game_rooms:" + roomId, "bet_point"));
+        roomJoinResDto.setTimeLimit((Integer) redisTemplate.opsForHash().get("game_rooms:" + roomId, "time_limit"));
+        return roomJoinResDto;
+    }
+
+    public GameStartResDto startGame(String roomId) {
+        redisTemplate.opsForHash().put("game_rooms:" + roomId, "game_status", "game_started");
+
+        GameStartResDto gameStartResDto = new GameStartResDto();
+        gameStartResDto.setRoomId(roomId);
+        return gameStartResDto;
+    }
+}
